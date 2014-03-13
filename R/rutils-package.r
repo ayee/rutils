8be9f2e8c6d778ec35d003297b15fc5e38bb847f
Anthony Yee <smyee@yahoo.com>
2014-03-13 15:32:30 -0700

    corrected DESCRIPTION

--- R/rutils-package.r
+++ R/rutils-package.r
@@ -12,8 +12,40 @@ setup_twitter <- function() {
   requestURL <- "https://api.twitter.com/oauth/request_token"
   accessURL <- "https://api.twitter.com/oauth/access_token"
   authURL <- "https://api.twitter.com/oauth/authorize"
-  consumerKey <- "31IR5vXasfNNMO4vZpBkw"
-  consumerSecret <- "Nps3lhbITl4ssjbpVMD9ni1tjnzDly58UBNSvFcmzs"
+  consumerKey <- "61VrNcuSiykRDUeICRKBGw"
+  consumerSecret <- "ST50iEVwZkapnrlTuPqjSHtIomRRyjpN53sFjtA"
   twitCred <- OAuthFactory$new(consumerKey=consumerKey,consumerSecret=consumerSecret,requestURL=requestURL,accessURL=accessURL,authURL=authURL)
   twitCred$handshake()
+  registerTwitterOAuth(twitCred)
 }
+
+# improved list of objects
+.ls.objects <- function (pos = 1, pattern, order.by,
+                         decreasing=FALSE, head=FALSE, n=5) {
+  napply <- function(names, fn) sapply(names, function(x)
+    fn(get(x, pos = pos)))
+  names <- ls(pos = pos, pattern = pattern)
+  obj.class <- napply(names, function(x) as.character(class(x))[1])
+  obj.mode <- napply(names, mode)
+  obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+  obj.prettysize <- napply(names, function(x) {
+    capture.output(print(object.size(x), units = "auto")) })
+  obj.size <- napply(names, object.size)
+  obj.dim <- t(napply(names, function(x)
+    as.numeric(dim(x))[1:2]))
+  vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+  obj.dim[vec, 1] <- napply(names, length)[vec]
+  out <- data.frame(obj.type, obj.size, obj.prettysize, obj.dim)
+  names(out) <- c("Type", "Size", "PrettySize", "Rows", "Columns")
+  if (!missing(order.by))
+    out <- out[order(out[[order.by]], decreasing=decreasing), ]
+  if (head)
+    out <- head(out, n)
+  out
+}
+
+# shorthand
+lsos <- function(..., n=10) {
+  .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+}
+
